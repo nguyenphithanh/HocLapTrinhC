bbfedd2a5506751896571736f0a5a57c67b9b077
thanhphi2001 <nguyenphithanhitk60@gmail.com>
2020-04-04 13:10:40 +0700

    #1: add File

---
+++ KienThuc/README.md
@@ -0,0 +1,36 @@
+# variable
+# keyword
+# switch... case
+# if/else if/ else
+# loop
+- for loop
+- while loop
+- do...while loop
+# struct
+# pointer
+# Data Structure
+# class
+# OOP
+# Database
+- text
+- sqlServer
+- MySql
+- MongoDB
+- Oracle DB
+## array
+- a[]
+- a[][]
+# Algorithms
+- create
+- research
+# web
+- front end
+- back end
+- Network
+# techniques
+- dat linh canh
+- de quy
+- nhap xuat mang
+- debug
+- phan tach ham
+- dat bien
---
+++ kyThuatNhapXuat/nhapXuat.cpp
@@ -0,0 +1,46 @@
+// nhap xuat bien
+// nhap xuat mang
+// Mang 1 chieu
+// Mang 2 chieu
+// nhap xuat struct
+// Nhap Tu file
+#include<stdio.h>
+typedef struct Diem
+{
+    /* data */
+};
+
+void nhapBien(int &a){
+    // bat ngoai le
+}
+void nhapDiem(Diem diem){
+
+}
+void nhapMangMotChieu(int *a, int &n){
+
+}
+void nhapMangHaiChieu(int **a, int &m, int &n){
+
+}
+void NhapTuFile(FILE *file, char *path, int *a, int &n){
+
+}
+void xuatBien(int a){
+    // dinh dang xuat
+
+}
+void xuatDiem(Diem diem){
+
+}
+void xuatMangMotChieu(int a[],int n){
+
+}
+void xuatMangHaiChieu(int a[100][100],int m, int n ){
+
+}
+void xuatDenFile(FILE *file, char *path, int a[100], int n){
+
+}
+int main(){
+ return 0;   
+}
